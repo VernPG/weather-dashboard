9eab8189a2de2226bc6e99d114c239a94ee1ea79
Veronica Perez <veperez@live.com>
2023-10-12 10:01:18 -0500

    html setup

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,26 @@
 var city=""
-var searchBtn= "";
-var listGroup= ""'
+var searchBtn= ".btn";
+var listGroup= "";
+var futureFore= $("#future-forecast");
+
+//search for city results
+function getApi () {
+    var requestUrl="f2165315a927629b82c1512a361fb24c";
+    fetch(requestUrl)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data[0].html_url)
+        // const pTag
+        }
+    )
+};
+
+//enter api 
+
+//call the current weather
+
+//display 5 day outlook
+
+//save search history
