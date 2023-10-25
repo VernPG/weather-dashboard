4360d50001e45c37d9520d7c08aa3b70948ee500
Veronica Perez <veperez@live.com>
2023-10-25 17:58:48 -0500

    fixed displayWeatherData function

--- assets/script1.js
+++ assets/script1.js
@@ -13,7 +13,7 @@ function getApi(lat, lon) {
     lat +
     "&lon=" +
     lon +
-    "&units=imperial&appid=f2165315a927629b82c1512a361fb24c";
+    "&appid=f2165315a927629b82c1512a361fb24c";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -24,7 +24,7 @@ function getApi(lat, lon) {
       for (var i = 0; i < data.list.length; i += 8) {
         // var listItem = document.createElement("li");
         console.log("Loop: " + i, data.list[i]);
-        displayWeatherData(data.list[i], i);
+        displayWeatherData(data.list[i]);
         // listItem.textContent = data.list[i].main.humidity;
         // console.log("PRV DATA: ", data.list[i].main.humidity);
         // weatherCard.appendChild(listItem);
@@ -39,7 +39,7 @@ function getLocation() {
   var requestUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
     city +
-    "&units=imperial&appid=f2165315a927629b82c1512a361fb24c";
+    "&appid=f2165315a927629b82c1512a361fb24c";
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -78,57 +78,20 @@ function displayData() {
         return data;
       })
       .then((data) => {
-        data.map((item, i) => {
-          console.log(i)
-            displayWeatherData(item, i);
-          
+        data.forEach((item) => {
+          displayWeatherData(item);
         });
       });
   });
 }
-function displayWeatherData(data, i) {
-  var todayDiv = document.getElementById('today-weather');
-  var forecastDiv = document.getElementById('forecast-weather')
-  var div = document.createElement("div")
-  div.className = i === 0 ? "col-sm-8" : "col-sm-3"
-  div.innerHTML = i === 0 ? "Today's Weather" : ''
- console.log(i)
-
- data.temp + 39 * 5
-  
-  //this gets repeated for each p tag
-  var dateEl = document.createElement("p")
-  dateEl.innerHTML = data.dt_txt
-  div.appendChild(dateEl)
-  // div.innerHTML = `<p>${data.dt_txt}</p>
-  //                  <p>${data.weather[0].description}</p>
-  //                  <p>Temp: ${data.main.temp}</p>
-  //                  <p>Wind: ${data.wind.speed}</p>
-  //                  <p>Humidity: ${data.main.humidity}</p>`;
-  if (i === 0) {
-    todayDiv.appendChild(div)
-
-  } else {
-    forecastDiv.appendChild(div)
-
-  }
+function displayWeatherData(data) {
+  var city = document.getElementById("weather-card");
+  var div = document.createElement("div");
+  div.className = "weather-card";
+  div.innerHTML = ` <p${data.dt_txt} id="date0" ></p$>
+                    <p i${data.weather[0].description}></p>
+                    <p>Temp:${data.main.temp}<span id="temp0"></span></p>
+                    <p> Wind:${data.wind.speed}<span id="wind0"></span></p>
+                    <p>Humidty:${data.main.humidity}<span id="humid0"></span></p>`;
+  city.appendChild(div);
 }
-
-
-// get api
-// var apiKey = "f2165315a927629b82c1512a361fb24c";
-//local storage
-// fetchButton.addEventListener("click", function(event){
-//   event.preventDefault();
-
-//   var dataListOptions ={
-//     city: city.value
-// };
-
-// localStorage.setItem("dataListOptions", JSON.stringify(dataListOptions));
-// renderMessage();
-// });
-// function renderMessage(){
-//   var city = JSON.parse(localStorage.getItem("dataListOptions"));
-
-// }
