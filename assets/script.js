c683d4ddc9eaef0c01fb046a420de38c92273109
Veronica Perez <veperez@live.com>
2023-10-29 15:38:24 -0500

    added in final reademe screenshot and css styling

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,8 @@ var weatherData = [];
 var fetchButton = document.getElementById("fetch-button");
 var currCity = document.getElementById("currCity");
 
+
+
 function getApi(lat, lon) {
   console.log(lat);
   console.log(lon);
@@ -93,38 +95,38 @@ function displayWeatherData(data, i) {
   var div = document.createElement("div")
   // div.setAttribute("style", "width: 18rem;")
 
-  div.className = i === 0 ? "col-sm-8 " : "forecast-weather";
-  div.innerHTML = i === 0 ? "Five Day Forecast" : ''
+  div.className = i === 0 ? "today-weather " : "forecast-weather";
+  div.innerHTML = i === 0 ? "Five Day Forecast Starting with TODAY in the City of:" : ''
  console.log(i)
 
  
   //this gets repeated for each p tag
   var nameEl = document.createElement("p")
-  nameEl.innerHTML = `City Name: ${currCity.value}`;
+  nameEl.innerHTML = `${currCity.value}`;
   div.appendChild(nameEl)
 
+  var imgEl = document.createElement("img")
+  imgEl.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`)
+  div.appendChild(imgEl)
+
   var dateEl = document.createElement("p")
-  dateEl.innerHTML = data.dt_txt
+  dateEl.innerHTML = `Date: ${data.dt_txt}`
   div.appendChild(dateEl)
 
-  var descriptionEl = document.createElement("p")
-  descriptionEl.innerHTML = data.weather[0].description
-  div.appendChild(descriptionEl)
+  // var descriptionEl = document.createElement("p")
+  // descriptionEl.innerHTML = data.weather[0].description
+  // div.appendChild(descriptionEl)
 
   var tempEl = document.createElement("p")
   tempEl.innerHTML = `Temp: ${data.main.temp}°F`
   div.appendChild(tempEl)
 
   var windEl = document.createElement("p")
-  windEl.innerHTML = data.wind.speed
+  windEl.innerHTML = `Wind Speed: ${data.wind.speed}mph`
   div.appendChild(windEl)
 
-  var imgEl = document.createElement("img")
-  imgEl.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`)
-  div.appendChild(imgEl)
-
   var humidityEl = document.createElement("p")
-  humidityEl.innerHTML = data.main.humidity
+  humidityEl.innerHTML = `Humidty: ${data.main.humidity}%`
   div.appendChild(humidityEl)
   // div.innerHTML = `<p>${data.dt_txt}</p>
   //                  <p>${data.weather[0].description}</p>
@@ -141,20 +143,26 @@ function displayWeatherData(data, i) {
 }
 
 
-// get api
-// var apiKey = "f2165315a927629b82c1512a361fb24c";
 //local storage
-// fetchButton.addEventListener("click", function(event){
-//   event.preventDefault();
+function readFromLocalStorage() {  
+  savedSearches = JSON.parse(localStorage.getItem('petspace-saved-searches')) || [];
+}
 
-//   var dataListOptions ={
-//     city: city.value
-// };
+function saveToLocalStorage() {
+  readFromLocalStorage();
+  
+  // new search string
+  var newItem = {
+      displayStr: storeString,
+      queryStr: queryString,
+  }
 
-// localStorage.setItem("dataListOptions", JSON.stringify(dataListOptions));
-// renderMessage();
-// });
-// function renderMessage(){
-//   var city = JSON.parse(localStorage.getItem("dataListOptions"));
-
-// }
+  // Compare to existing and only add new unique searches
+  var isNewSearch = (savedSearches.filter(savedSearches => savedSearches.queryStr == newItem.queryStr).length === 0);
+  if(isNewSearch){
+      savedSearches.push(newItem);
+      localStorage.setItem('petspace-saved-searches', JSON.stringify(savedSearches)); 
+  } else {
+      console.log("Not a new search parameters...");
+  }
+}
